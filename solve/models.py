912c8672d50c67fbedb7f68a7949c03d9865cb4f
DavidNganga <ngashiedavid@gmail.com>
2018-05-30 12:57:16 +0300

    created models

--- solve/models.py
+++ solve/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Error(models.Model):
+    language = models.CharField(max_length = 25,null = True)
+    error = models.CharField(max_length=250,null=True)
+    explanation = models.CharField(max_length=200,null=True)
+
+class Comment(models.Models):
+    error = models.ForeignKey(Error, on_delete=models.CASCADE,null=True)
+    post = models.CharField(max_length=200,null=True)
