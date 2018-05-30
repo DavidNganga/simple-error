337ddce9b4e59e264da189e88a2e8e5b45bddd71
DavidNganga <ngashiedavid@gmail.com>
2018-05-30 13:12:42 +0300

    created a templates folder

--- solve/views.py
+++ solve/views.py
@@ -3,9 +3,16 @@ from .models import Comment,error
 
 # Create your views here.
 def error(request):
-
-
-
-
-    
-    return(request, 'error.html')
+    # current_user = request.user.id
+    # user_instance =User.objects.get(id=request.user.id)
+    if request.method == 'POST':
+        form = ErrorForm(request.POST, request.FILES)
+        if form.is_valid():
+            error = form.save(commit=False)
+            # error.profile__user = current_user
+            # error.user=user_instance
+            error.save()
+            return redirect('welcome')
+    else:
+        form = ErrorForm()
+    return render(request, 'error.html', {"form": form})
