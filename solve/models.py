6e2990a1aeb635781af3cf08803a21c20f101927
DavidNganga <ngashiedavid@gmail.com>
2018-05-31 12:32:00 +0300

    created superuser

--- solve/models.py
+++ solve/models.py
@@ -4,8 +4,8 @@ from django.db import models
 class Error(models.Model):
     language = models.CharField(max_length = 25,null = True)
     error = models.CharField(max_length=250,null=True)
-    explanation = models.CharField(max_length=200,null=True)
+    explanation = models.TextField(max_length=200,null=True)
 
 class Comment(models.Model):
     error = models.ForeignKey(Error, on_delete=models.CASCADE,null=True)
-    post = models.CharField(max_length=200,null=True)
+    post = models.TextField(max_length=200,null=True)
